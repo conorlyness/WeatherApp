bba80382ba5336f43257937182be35db8f16181c
Conor Lyness <conorlyness@gmail.com>
2022-05-29 12:23:02 +0100

    add the service to app component and call it

--- weather-app/src/app/app.component.ts
+++ weather-app/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { weatherData } from './models/weather.model';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'weather-app';
+export class AppComponent implements OnInit {
+  constructor(private weatherService: WeatherService) {}
+
+  weatherData?: weatherData;
+
+  ngOnInit(): void {
+    this.weatherService.getWeatherData('Ballymena').subscribe({
+      next: (response) => {
+        this.weatherData = response;
+        console.log(response);
+      },
+    });
+  }
 }
